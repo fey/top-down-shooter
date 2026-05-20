a79c582394dd85742b0e796187cddbc95d4820a5
Nikolay Gagarinov <feycot@gmail.com>
2026-05-21 03:05:00 +0500

    feat(M3): generate bullet texture in PreloadScene

--- src/scenes/PreloadScene.ts
+++ src/scenes/PreloadScene.ts
@@ -7,9 +7,15 @@ export class PreloadScene extends Phaser.Scene {
 
   create(): void {
     const gfx = this.add.graphics();
+
     gfx.fillStyle(0xffffff, 1).fillRect(0, 0, 32, 32);
     gfx.fillStyle(0x000000, 1).fillTriangle(16, 2, 10, 14, 22, 14);
     gfx.generateTexture("player", 32, 32);
+
+    gfx.clear();
+    gfx.fillStyle(0xffff88, 1).fillRect(0, 0, 8, 4);
+    gfx.generateTexture("bullet", 8, 4);
+
     gfx.destroy();
 
     this.scene.start("Game");
