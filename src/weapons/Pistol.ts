869e6fa42f0c5b3b6d9e859d750d7034add5ed89
Nikolay Gagarinov <feycot@gmail.com>
2026-05-21 03:21:03 +0500

    feat(M3): add Pistol weapon

---
+++ src/weapons/Pistol.ts
@@ -0,0 +1,22 @@
+import Phaser from "phaser";
+import { BULLET_SPEED, PISTOL_COOLDOWN } from "../config";
+import { Bullet } from "../entities/Bullet";
+import { Weapon } from "./Weapon";
+
+export class Pistol extends Weapon {
+  constructor() {
+    super(PISTOL_COOLDOWN, BULLET_SPEED);
+  }
+
+  protected spawnBullets(
+    bulletGroup: Phaser.Physics.Arcade.Group,
+    x: number,
+    y: number,
+    angle: number,
+  ): void {
+    const vx = Math.cos(angle) * this.bulletSpeed;
+    const vy = Math.sin(angle) * this.bulletSpeed;
+    const bullet = new Bullet(bulletGroup.scene, x, y, vx, vy);
+    bulletGroup.add(bullet);
+  }
+}
