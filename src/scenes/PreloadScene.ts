8a9d786f3759063b34ad7c63d4e5121f7d23bbbd
Nikolay Gagarinov <feycot@gmail.com>
2026-05-21 03:17:43 +0500

    fix: generate enemy_melee texture in PreloadScene

--- src/scenes/PreloadScene.ts
+++ src/scenes/PreloadScene.ts
@@ -16,6 +16,10 @@ export class PreloadScene extends Phaser.Scene {
     gfx.fillStyle(0xffff88, 1).fillRect(0, 0, 8, 4);
     gfx.generateTexture("bullet", 8, 4);
 
+    gfx.clear();
+    gfx.fillStyle(0xff4444, 1).fillRect(0, 0, 32, 32);
+    gfx.generateTexture("enemy_melee", 32, 32);
+
     gfx.destroy();
 
     this.scene.start("Game");
