e91f201b83919456bcb78bc2106ef94fe538745b
Nikolay Gagarinov <feycot@gmail.com>
2026-05-21 03:03:44 +0500

    feat(M3): add bullet/pistol constants to config

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,7 @@
 export const PLAYER_SPEED = 200;
 export const MAP_WIDTH = 1920;
 export const MAP_HEIGHT = 1080;
+
+export const BULLET_SPEED = 600;
+export const BULLET_TTL = 2000;
+export const PISTOL_COOLDOWN = 250;
