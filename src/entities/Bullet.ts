da4d7bc430ee338f9d14903a93e0efad4f2c6487
Nikolay Gagarinov <feycot@gmail.com>
2026-05-21 03:09:39 +0500

    Revert "M4: add damage field to Bullet"
    This reverts commit 3c1cf3dd4227cb09e534405c512f71248f3bd422.

--- src/entities/Bullet.ts
+++ src/entities/Bullet.ts
@@ -2,13 +2,10 @@ import Phaser from "phaser";
 import { BULLET_TTL } from "../config";
 
 export class Bullet extends Phaser.Physics.Arcade.Sprite {
-  damage: number;
-
-  constructor(scene: Phaser.Scene, x: number, y: number, vx: number, vy: number, damage = 1) {
+  constructor(scene: Phaser.Scene, x: number, y: number, vx: number, vy: number) {
     super(scene, x, y, "bullet");
     scene.add.existing(this);
     scene.physics.add.existing(this);
-    this.damage = damage;
     this.setVelocity(vx, vy);
     scene.time.addEvent({
       delay: BULLET_TTL,
