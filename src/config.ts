967ff6b70928417b5683fbe70e935b17af0a1df5
Nikolay Gagarinov <feycot@gmail.com>
2026-05-21 03:07:54 +0500

    Revert "M4: add enemy and player HP constants to config"
    This reverts commit 2aeb48844a10dafb1799a782cf2aa31299fef5e2.

--- src/config.ts
+++ src/config.ts
@@ -5,10 +5,3 @@ export const MAP_HEIGHT = 1080;
 export const BULLET_SPEED = 600;
 export const BULLET_TTL = 2000;
 export const PISTOL_COOLDOWN = 250;
-
-export const PLAYER_HP = 5;
-
-export const MELEE_ENEMY_HP = 2;
-export const MELEE_ENEMY_SPEED = 140;
-export const MELEE_ENEMY_DAMAGE = 1;
-export const MELEE_ENEMY_ATTACK_COOLDOWN = 600;
